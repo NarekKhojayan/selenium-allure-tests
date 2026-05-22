af1f980853d03b71fee126d986d7266ede67333f
Narek <Narek@DESKTOP-9EMGVQL>
2026-05-22 17:45:19 +0400

    Chrome Driver change

--- WeatherTests.UI/Core/DriverFactory.cs
+++ WeatherTests.UI/Core/DriverFactory.cs
@@ -1,6 +1,5 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
 public static class DriverFactory
 {
     public static IWebDriver GetDriver()
@@ -8,6 +7,7 @@ public static class DriverFactory
         var options = new ChromeOptions();
         options.AddArgument("--no-sandbox");
         options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument("--headless");
         return new ChromeDriver(options);
     }
 }
